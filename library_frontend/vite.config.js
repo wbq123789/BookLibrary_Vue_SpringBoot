46083123a42cf88d850a4d3e095435366e70def4
Flying <1724593655@qq.com>
2023-12-22 17:24:13 +0800

    Fixing configuration errors

--- library_frontend/vite.config.js
+++ library_frontend/vite.config.js
@@ -1,16 +1,22 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { createApp } from 'vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import App from './App.vue'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
-const app = createApp(App)
-
-app.use(ElementPlus)
-app.mount('#app')
-
+// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
